0f7b3cf32fb7e841e2fee5788e3b5b951830522c
Roni Frantchi <roni.frantchi@gmail.com>
2017-07-04 11:12:12 +0300

    poc of how to apply classes

--- src/lib/src/component/form-control.component.ts
+++ src/lib/src/component/form-control.component.ts
@@ -1,15 +1,22 @@
-import {Component, ContentChild, OnInit} from '@angular/core';
+import {Component, ContentChild, Input, OnInit} from '@angular/core';
 import {RequiredValidator} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'form-control',
-  template: `<ng-content></ng-content> <span *ngIf="required">*</span>`
+  template: `
+    <div ngClass="hf-field,{'hf-field--required': required}">
+      <label class="hf-field__label">{{label}}</label>
+      <span><ng-content></ng-content></span>
+    </div>`
 })
 export class FormControlComponent implements OnInit {
 
   @ContentChild(RequiredValidator)
   requiredValidator: RequiredValidator;
+
+  @Input()
+  label: string;
 
   get required() {
     return this.requiredValidator && this.requiredValidator.required;
