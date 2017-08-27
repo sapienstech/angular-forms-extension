a010d7daa03730bd0f23a312be8d70e3836bbe56
Roni Frantchi <roni.frantchi@gmail.com>
2017-08-27 13:46:46 +0300

    fixed validation issue

--- src/lib/src/component/form-control.component.ts
+++ src/lib/src/component/form-control.component.ts
@@ -36,7 +36,7 @@ export class FormControlComponent implements AfterContentInit {
 
   get valid() {
     return this.formControlNameDirective &&
-      (this.formControlNameDirective.groupSubmitted && this.formControlNameDirective.pristine || this.formControlNameDirective.valid);
+      (!this.formControlNameDirective.groupSubmitted && this.formControlNameDirective.pristine || this.formControlNameDirective.valid);
   }
 
   get errors() {
