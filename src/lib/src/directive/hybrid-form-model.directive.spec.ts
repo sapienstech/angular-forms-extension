fcb18a78df5f5429ee4735e3190642950d9174f6
Roni Frantchi <roni.frantchi@gmail.com>
2017-08-31 22:06:05 +0300

    added first batch of tests

--- src/lib/src/directive/hybrid-form-model.directive.spec.ts
+++ src/lib/src/directive/hybrid-form-model.directive.spec.ts
@@ -1,25 +1,63 @@
 import {HybridForm} from './hybrid-form.directive';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HybridFormsModule} from '../hybrid-forms.module';
+import {Component, ViewChild} from '@angular/core';
+import {HybridFormModelDirective} from './hybrid-form-model.directive';
+import {NgModel} from '@angular/forms';
 
 describe('HybridFormModelDirective', () => {
 
-  it('should target the host [NgModel]', () => {
+  @Component({
+    template: `
+      <form>
+        <input [(ngModel)]="value" [name]="'input'" required>
+      </form>`
+  })
+  class TestComponent {
+    @ViewChild(HybridFormModelDirective) hybridFormModel: HybridFormModelDirective;
+    @ViewChild(NgModel) ngModel: NgModel;
+    value;
+  }
 
+  let fixture: ComponentFixture<TestComponent>;
+  let instance: TestComponent;
+  let hybridFormModel: HybridFormModelDirective;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({imports: [HybridFormsModule], declarations: [TestComponent]})
+      .createComponent(TestComponent);
+    instance = fixture.componentInstance;
+    hybridFormModel = instance.hybridFormModel;
   });
 
-  it('should allow access to the host [NgModel] name', () => {
+  beforeEach(async(() => fixture.detectChanges()));
 
-  });
+  it('should target the host [ngModel]', () =>
+    expect(hybridFormModel).toBeTruthy()
+  );
 
-  it('should allow access to the host [NgModel] validity', () => {
+  it('should allow access to the host [ngModel] name', () =>
+    expect(hybridFormModel.name).toBe('input')
+  );
 
-  });
+  [true, false].forEach(valid =>
+    it(`should ${valid ? '' : 'NOT'} be valid when [ngModel] is ${valid ? '' : 'NOT'}`, async(() => {
+      instance.value = valid ? 'filling in a required field' : '';
+      fixture.detectChanges();
+      fixture.whenStable().then(() => expect(hybridFormModel.valid).toBe(valid));
+    })));
 
-  it('should allow access to the host [NgModel] pristineness', () => {
+  [true, false].forEach(pristine =>
+    it(`should ${pristine ? '' : 'NOT'} be pristine when [ngModel] is ${pristine ? '' : 'NOT'}`, () => {
+      instance.value = !pristine ? 'no longer pristine!' : instance.value;
+      fixture.detectChanges();
+      fixture.whenStable().then(() => expect(hybridFormModel.pristine).toBe(pristine));
+    }));
 
-  });
-
-  it('should allow access to the host [NgModel] errors', () => {
-
+  it('should allow access to the host [ngModel] errors', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() =>
+      expect(hybridFormModel.errors).toBe(instance.ngModel.control.errors));
   });
 
   describe('when the model has changed', () => {
