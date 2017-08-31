b5836fb2480041ceaafb19710a57526236a20b10
Roni Frantchi <roni.frantchi@gmail.com>
2017-08-31 10:47:44 +0300

    added proper directive exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,4 @@
 export {HybridFormsModule} from './src/hybrid-forms.module';
 export {HybridForm} from './src/directive/hybrid-form.directive'
+export {UnsavedDirective} from './src/directive/unsaved.directive'
+export {ValidSubmitDirective} from './src/directive/valid-submit.directive'
