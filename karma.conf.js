faae30f549745abc614df9460e011b8cc554e8d6
Roni Frantchi <roni.frantchi@gmail.com>
2017-09-02 23:56:49 +0300

    fixed junit path for circleci!

--- karma.conf.js
+++ karma.conf.js
@@ -81,7 +81,7 @@ module.exports = function (config) {
     reporters: process.env.CI ? ['junit','dots'] : ['progress', 'kjhtml'],
 
     junitReporter: {
-      outputDir: process.env['$CIRCLE_TEST_REPORTS'].concat('/junit/`),
+      outputDir: `${process.env.$CIRCLE_TEST_REPORTS}/junit/`,
       outputFile: 'test-results.xml',
       useBrowserName: false
     },
