a8c11679c340d34e2ef46403120198e4a2c05e8f
Roni Frantchi <roni.frantchi@gmail.com>
2017-09-12 11:29:47 +0300

    exported message service

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
-export {FormsExtensionModule} from './src/forms-extension.module';
+export {FormsExtensionModule} from './src/forms-extension.module'
 export {FxForm} from './src/directive/fx-form.directive'
 export {UnsavedDirective} from './src/directive/unsaved.directive'
 export {ValidSubmitDirective} from './src/directive/valid-submit.directive'
+export {FormValidationMessageService} from './src/service/form-validation-message.service'
