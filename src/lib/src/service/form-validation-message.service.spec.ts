e0bc0c3593d8fa1aebdfacaac1a810edd2473c79
brachi-wernick <brachi.w@sapiens.com>
2017-10-01 15:13:41 +0300

    fix test

--- src/lib/src/service/form-validation-message.service.spec.ts
+++ src/lib/src/service/form-validation-message.service.spec.ts
@@ -8,33 +8,34 @@ describe('FormValidationMessageService', () => {
   [true, false].forEach(appendFieldNameToMessage => {
     describe(`when 'appendFieldNameToMessage' set to ${appendFieldNameToMessage}`, () => {
 
-      let field: string;
+      const field='Email';
+      let fieldInMessage:string;
 
       beforeEach(() => {
         service.appendFieldNameToMessage = appendFieldNameToMessage;
-        appendFieldNameToMessage ? field = 'Email ' : field = "";
+        appendFieldNameToMessage ? fieldInMessage = '${field} ' : fieldInMessage = "";
       });
 
       describe('default messages', () => {
         it(`should say 'is required' when required`, () =>
           expect(service.getErrorMessage(field, 'required', {required: true}))
-            .toBe(`${field}is required`));
+            .toBe(`${fieldInMessage}is required`));
 
         it(`should say 'must be at least {{requiredLength}} characters long' when minlength is set`, () =>
           expect(service.getErrorMessage(field, 'minlength', {
             actualLength: 1,
             requiredLength: 5
-          })).toBe(`${field}must be at least 5 characters long`));
+          })).toBe(`${fieldInMessage}must be at least 5 characters long`));
 
         it(`should say 'no more than {{requiredLength}} characters long' when maxlength is set`, () =>
           expect(service.getErrorMessage(field, 'maxlength', {
             actualLength: 1,
             requiredLength: 10
-          })).toBe(`${field}must be no more than 10 characters long`));
+          })).toBe(`${fieldInMessage}must be no more than 10 characters long`));
 
         it(`should say 'Email must be valid' when email field`, () =>
           expect(service.getErrorMessage(field, 'email', {email: true}))
-            .toBe(`${field}must be valid`));
+            .toBe(`${fieldInMessage}must be valid`));
       });
 
       describe('adding messages', () => {
@@ -42,11 +43,11 @@ describe('FormValidationMessageService', () => {
 
         it('should show "Email has to start with somethingood"', () =>
           expect(service.getErrorMessage(field, 'startsWith', {beginning: 'somethinggood'}))
-            .toBe(`${field}has to start with somethinggood`));
+            .toBe(`${fieldInMessage}has to start with somethinggood`));
 
         it('should NOT get rid of the default messages', () =>
           expect(service.getErrorMessage(field, 'required', {required: true}))
-            .toBe(`${field}is required`));
+            .toBe(`${fieldInMessage}is required`));
       });
 
       describe('overriding messages', () => {
@@ -54,7 +55,7 @@ describe('FormValidationMessageService', () => {
 
         it('should should override the message for required', () =>
           expect(service.getErrorMessage(field, 'required', {required: true}))
-            .toBe(`${field}can't be blank`));
+            .toBe(`${fieldInMessage}can't be blank`));
       });
     });
 
